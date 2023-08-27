6811f44f1e1cdfcbcdb564043563abbd8a8f47ac
HMFazleRabbi <f.rabbiarcs@gmail.com>
2023-08-27 15:18:57 +0600

    Added transfer test and fixtures

--- test/Token.js
+++ test/Token.js
@@ -2,12 +2,28 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Token contract", function () {
+
+    async function deployTokenFixture() {
+        const [owner, addr1, addr2] = await ethers.getSigners();
+        const hardhatToken = await ethers.deployContract("Token");
+
+        return {hardhatToken, owner, addr1, addr2}
+
+    }
+
+
+
+
+
+
   it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners();
-
-    const hardhatToken = await ethers.deployContract("Token");
-
+    const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
     const ownerBalance = await hardhatToken.balanceOf(owner.address);
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
   });
+
+
+
+
+
 });
